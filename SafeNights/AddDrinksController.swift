6337657925774e642094d3bad15440736afabf41
okkhoury <okk6nb@virginia.edu>
2017-04-09 14:41:39 -0400

    Put in the code and interface to add drinks. The request is setup, but i'm not sure what to do for the current date, so just need to add that and that functionality will be done

--- SafeNights/AddDrinksController.swift
+++ SafeNights/AddDrinksController.swift
@@ -16,16 +16,39 @@ class AddDrinksController: UIViewController {
     @IBOutlet var WineTextEdit: UITextField!
     @IBOutlet var BeerTextEdit: UITextField!
     
-    @IBOutlet var SubmitButton: UIButton!
+    
+    @IBOutlet var submitButton: UIButton!
     
     let API = MyAPI()
+
+    let username = mainInstance.username
+    let password = mainInstance.password
+   
+    // Get the current day, month, year
+    let date = NSDate()
+    let calendar = NSCalendar.current
     
-    @IBAction func clickCreateAccount(_ sender: Any) {
+    //Not sure what to do about the date. what form should it be in
+    
+   
+    @IBAction func clickSubmit(_ sender: Any) {
         let resource = API.addDrinks
         
-        let beerAmount = BeerTextEdit.text
+        // Need to add the rest of the fields
+        let postData = ["username":username, "pwd":password]
         
-        
-        
+        resource.request(.post, urlEncoded: postData).onSuccess() { data in
+            
+            // This code gets the response from the user in the form ["passed": 'y'/'n']
+            var response = data.jsonDict
+            let responseAnswer = response["passed"]
+            
+            //startNightAnswer either returns a unique night ID or 'n'(failed)
+            if let responseAnswer = responseAnswer as? String, responseAnswer == "y" {
+                print("Drinks added")
+            } else if let responseAnswer = responseAnswer as? String, responseAnswer == "n" {
+                print("Drinks not added")
+            }
+        }
     }
 }
