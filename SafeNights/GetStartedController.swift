01b41efea5e335e97497b16efdec70f776f0dbad
okkhoury <okk6nb@virginia.edu>
2017-05-21 01:44:35 -0400

    Added the post request to the start night page for getting a night id

--- SafeNights/GetStartedController.swift
+++ SafeNights/GetStartedController.swift
@@ -15,21 +15,34 @@ class GetStartedController: UIViewController {
     @IBOutlet var contactTextField: UITextField!
     @IBOutlet var emailTextField: UITextField!
     
+    
     @IBOutlet var submitButton: UIButton!
     
+    let API = MyAPI()
     
-    @IBAction func submitAction(_ sender: Any) {
-        
+    
+    @IBAction func submit(_ sender: Any) {
         let username = mainInstance.username
         let password = mainInstance.password
         
-        let API = MyAPI()
         let resource = API.startNight
+        let postData = ["username": username, "pwd": password]
         
-        
-        
-        
-        
+        // Make request to database to get a new adventureID (I called it a nightID)
+        resource.request(.post, urlEncoded: postData).onSuccess() { data in
+            
+            // This code gets the response from the user in the form ["passed": 'y'/'n']
+            var response = data.jsonDict
+            let startNightAnswer = response["passed"]
+            
+            //startNightAnswer either returns a unique night ID or 'n'(failed)
+            if let startNightAnswer = startNightAnswer as? String, startNightAnswer != "n" {
+                print(startNightAnswer)
+                mainInstance.nightID = startNightAnswer
+            } else if let startNightAnswer = startNightAnswer as? String, startNightAnswer == "n" {
+                print("night has not started")
+            }
+        }
         
     }
 }
