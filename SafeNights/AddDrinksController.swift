c421871426d034a8a5f8c9e3ddd39863036dc15e
okkhoury <okk6nb@virginia.edu>
2017-05-21 00:50:52 -0400

    Added the post request code to the add drinks page. Seems to correctly store the data now

--- SafeNights/AddDrinksController.swift
+++ SafeNights/AddDrinksController.swift
@@ -37,28 +37,28 @@ class AddDrinksController: UIViewController {
     }
     
     override func viewDidLoad() {
-        seekBar1.frame = CGRect(x: 30, y: 300, width: 100, height: 100)
+        seekBar1.frame = CGRect(x: 40, y: 355, width: 75, height: 75)
         seekBar1.startAngle = 120
         seekBar1.endAngle = 60
         seekBar1.currentAngle = 120
         seekBar1.addTarget(self, action: Selector(("seekBarDidChangeValue:")), for: .valueChanged)
         self.view.addSubview(seekBar1)
         
-        seekBar2.frame = CGRect(x: 230, y: 300, width: 100, height: 100)
+        seekBar2.frame = CGRect(x: 260, y: 355, width: 75, height: 75)
         seekBar2.startAngle = 120
         seekBar2.endAngle = 60
         seekBar2.currentAngle = 120
         seekBar2.addTarget(self, action: Selector(("seekBarDidChangeValue:")), for: .valueChanged)
         self.view.addSubview(seekBar2)
         
-        seekBar3.frame = CGRect(x: 30, y: 450, width: 100, height: 100)
+        seekBar3.frame = CGRect(x: 40, y: 465, width: 75, height: 75)
         seekBar3.startAngle = 120
         seekBar3.endAngle = 60
         seekBar3.currentAngle = 120
         seekBar3.addTarget(self, action: Selector(("seekBarDidChangeValue:")), for: .valueChanged)
         self.view.addSubview(seekBar3)
         
-        seekBar4.frame = CGRect(x: 230, y: 450, width: 100, height: 100)
+        seekBar4.frame = CGRect(x: 260, y: 465, width: 75, height: 75)
         seekBar4.startAngle = 120
         seekBar4.endAngle = 60
         seekBar4.currentAngle = 120
@@ -80,7 +80,57 @@ class AddDrinksController: UIViewController {
         
         print(selectedDate)
         print(seekBar1.currentAngle)
+        
+        let wineAmount = String(Int(seekBar1.currentAngle))
+        let beerAmount = String(Int(seekBar2.currentAngle))
+        let liquorAmount = String(Int(seekBar3.currentAngle))
+        let shotsAmount = String(Int(seekBar4.currentAngle))
+        let moneyAmount = String(Int(moneySlider.value))
+        
+        let API = MyAPI()
+        
+        let username = mainInstance.username
+        let password = mainInstance.password
+        
+        let postData = ["username":username, "pwd":password, "day": selectedDate,
+                        "money": moneyAmount,"shots": shotsAmount, "liquor":liquorAmount,
+                        "wine": wineAmount, "beer": beerAmount] as [String : Any]
+        
+        let resource = API.addDrinks
+        
+        resource.request(.post, urlEncoded: postData as! [String : String]).onSuccess() { data in
+            
+            // Get the response from the user in the form ["passed": 'y'/'n']
+            var response = data.jsonDict
+            let responseAnswer = response["passed"]
+            
+            // check if the data was correctly added
+            if let responseAnswer = responseAnswer as? String, responseAnswer == "y" {
+                print("Drinks added")
+            } else {
+                print("Drinks not added")
+            }
+            
+        }
+        
     }
+    
+    //        let postData = ["username":username, "pwd":password, "day": selectedDate, "money": MoneyTextEdit.text!, "shots": ShotsTextEdit.text!, "liquor": LiquerTextEdit.text!, "wine": WineTextEdit.text!, "beer": BeerTextEdit.text!]
+    //
+    //        resource.request(.post, urlEncoded: postData).onSuccess() { data in
+    //
+    //            // This code gets the response from the user in the form ["passed": 'y'/'n']
+    //            var response = data.jsonDict
+    //            let responseAnswer = response["passed"]
+    //
+    //            //startNightAnswer either returns a unique night ID or 'n'(failed)
+    //            if let responseAnswer = responseAnswer as? String, responseAnswer == "y" {
+    //                print("Drinks added")
+    //            } else if let responseAnswer = responseAnswer as? String, responseAnswer == "n" {
+    //                print("Drinks not added")
+    //            }
+    //        }
+    //    }
     
 //    @IBOutlet var MoneyTextEdit: UITextField!
 //    @IBOutlet var ShotsTextEdit: UITextField!
